878eee550e7c4dec09cec36ae1963fd69d8c7458
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2024-03-14 17:18:56 -0300

    movimentando peças

--- src/program/Programa.java
+++ src/program/Programa.java
@@ -1,13 +1,31 @@
 package program;
 
+import java.util.Scanner;
+
 import chessLayer.ChessMatch;
+import chessLayer.ChessPiece;
+import chessLayer.ChessPosition;
 
 public class Programa {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece caputredPiece = chessMatch.performChessMovie(source, target);
+		}
+		
 	}
 	
 }
